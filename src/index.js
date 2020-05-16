a3152d55daed32d0d0b65a980588cfb9b34bf8ac
Prayrit Khanna <prayritk@gmail.com>
2020-05-16 19:13:00 -0400

    Adding back the updated files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Navigation from './Navigation';
+import Header from './Header';
 import * as serviceWorker from './serviceWorker';
+import Hobbies from './Hobbies.js';
+import Projects from './Projects';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Navigation logoTitle="Prayrit Khanna"/>
+        <Header title="{Hello World!}" button="Start the Tour!"/>
+        <Hobbies/>
+        <Projects/>
+        <images/>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
